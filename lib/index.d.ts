7aef2fa3c25a487e7a1b829240ee8aff4e845c0c
FlippieCoetser <Admin@Scientific-Planet.com>
2015-12-25 19:04:44 +0100

    Add Miscellanous Commands

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,14 @@
 /// <reference path="../typings/tsd.d.ts" />
-declare class PN532 {
-    testProperty: string;
-    constructor(property: string);
-    testFunction(): void;
+export declare enum Miscellanous {
+    Diagnose = 0,
+    GetFirmwareVersion = 2,
+    GetGeneralStatus = 4,
+    ReadRegister = 6,
+    WriteRegister = 8,
+    ReadGPIO = 12,
+    WriteGPIO = 14,
+    SetSerialBaudRate = 16,
+    SetParameters = 18,
+    SAMConfiguration = 20,
+    PowerDown = 22,
 }
