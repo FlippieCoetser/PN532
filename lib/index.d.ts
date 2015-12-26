482d5ba52ea8989c33b1d0655bb3bffe4fa7ae48
FlippieCoetser <Admin@Scientific-Planet.com>
2015-12-26 20:41:54 +0100

    Update Commands and Error Codes

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -14,9 +14,52 @@ declare class PN532 {
             SAMConfiguration: number;
             PowerDown: number;
         };
+        RFCommunication: {
+            Configuration: number;
+            RegulationTest: number;
+        };
+        Initiator: {
+            JumpForDEP: number;
+            JumpForPSL: number;
+            ListPassiveTarget: number;
+            ATR: number;
+            PSL: number;
+            DataExchange: number;
+            CommunicateThry: number;
+            Deselect: number;
+            Release: number;
+            Select: number;
+            AutoPoll: number;
+        };
+        Target: {
+            InitAsTarget: number;
+            SetGeneralBytes: number;
+            GetData: number;
+            SetData: number;
+            SetMetaData: number;
+            GetInitiatorCommand: number;
+            ResponseToInitiator: number;
+            GetTargetStatus: number;
+        };
+    };
+    static Error: {
+        0X01: string;
+        0X02: string;
+        0X03: string;
+        0X04: string;
+        0X05: string;
+        0X06: string;
+        0X07: string;
+        0X09: string;
+        0X0A: string;
+        0X0B: string;
+        0X0D: string;
+        0X0E: string;
+        0X10: string;
     };
     testProperty: string;
     constructor(property: string);
     testFunction(): void;
     getFirmwareVersion(): void;
+    getErrorCode(code: number): void;
 }
