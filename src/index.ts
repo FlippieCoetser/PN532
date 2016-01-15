02dc564edd5817e0bcebe69625cab9fbbdf1244b
FlippieCoetser <Admin@Scientific-Planet.com>
2016-01-15 13:46:15 +0100

    Remove Test Function and Property

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 /// <reference path="../typings/tsd.d.ts" />
+interface diagnose {
+	Test: PN532.Tests;
+	Parameters?: number;
+}
+
+
 class PN532 {
 	// List of Commands // 
 	static Command = {
@@ -26,7 +32,7 @@ class PN532 {
 			ATR: 0X50,
 			PSL: 0X4E,
 			DataExchange: 0X40,
-			CommunicateThry: 0X42,
+			CommunicateThru: 0X42,
 			Deselect: 0X44,
 			Release: 0X52,
 			Select: 0X54,
@@ -44,39 +50,85 @@ class PN532 {
 		}
 	}
 
-	// List of Errors //
+	// Error Codes //
 	static Error = {
-		0X01: 'Time Out, the target has not answered',
-		0X02: 'A CRC error has been detected by the CIU',
-		0X03: 'A Parity error has been detected by the CIU',
-		0X04: 'During an anti-collision/select operation (ISO/IEC14443-3 Type A and ISO/ IEC18092 106 kbps passive mode), an erroneous Bit Count has been detected',
-		0X05: 'Framing error during Mifare operation',
-		0X06: 'An abnormal bit-collision has been detected during bit wise anti-collision at 106 kbps',
-		0X07: 'Communication buffer size insufficient',
-		0X09: 'RF Buffer overflow has been detected by the CIU (bit BufferOvfl of the register CIU_Error)',
-		0X0A: 'In active communication mode, the RF field has not been switched on in time by the counterpart (as defined in NFCIP - 1 standard)',
-		0X0B: 'RF Protocol error (cf. Error! Reference source not found., description of the CIU_Error register)',
-		0X0D: 'Temperature error: the internal temperature sensor has detected overheating, and therefore has automatically switched off the antenna drivers',
-		0X0E: 'Internal buffer overflow',
-		0X10: 'Invalid parameter (range, format, …)'
+		0X01: 'Time Out',
+		0X02: 'CRC Error',
+		0X03: 'Parity Error',
+		0X04: 'Bit Count Error',
+		0X05: 'Framing Error',
+		0X06: 'Bit-collision',
+		0X07: 'Buffer Size',
+		0X09: 'RF Buffer Overflow',
+		0X0A: 'In-active Communication',
+		0X0B: 'RF Protocol Error',
+		0X0D: 'Temperature Error',
+		0X0E: 'Internal Buffer Overflow',
+		0X10: 'Invalid Parameter',
+		0X12: 'Invalid Command',
+		0X13: 'Invalid Data Format',
+		0X14: 'Authentication Error',
+		0X23: 'UID Check Byte Error',
+		0X25: 'Invalid Device State',
+		0X26: 'Invalid Operation',
+		0X27: 'Invalid Command',
+		0X29: 'Target Released',
+		0X2A: 'Invalid ID',
+		0X2B: 'Card Removed',
+		0X2C: 'NFC Mismatch',
+		0X2D: 'Over Current',
+		0X2E: 'NAD Missing'
 	}
 
-	testProperty: string;
-
-	constructor(property: string) {
-		this.testProperty = property
+	static Status = {
+		Field: {
+			0X00: 'External RF Field NOT Present',
+			0X01: 'External RF Field Present'
+		},
+		ReceptionBitRate: {
+			0X00: '106 kbps',
+			0X01: '212 kbps',
+			0X02: '424 kbps'
+		},
+		TransmissionBitRate: {
+			0X00: '106 kbps',
+			0X01: '212 kbps',
+			0X02: '424 kbps'
+		},
+		ModulationType: {
+			0X00: 'Mifare Passive Mode',
+			0X10: 'FeliCa Passive Mode',
+			0X01: 'FeliCa Active Mode',
+			0X02: 'Innovision Jewel tag'
+		}
 	}
-	testFunction():void {
-		console.log(this.testProperty);
+
+	Diagnose(input: diagnose): Buffer {
+		console.log('Test:' + input.Test);
+		input.Parameters && console.log('Input:' + input.Parameters);	 
+		return new Buffer('test');
 	}
 
 	getFirmwareVersion():void {
-		console.log(PN532.Command.Miscellanous.GetFirmwareVersion.toString(16));
+		console.log(PN532.Command.Miscellanous.GetFirmwareVersion);
 	}
 
 	getErrorCode(code: number):void {
 		console.log(PN532.Error[code]);
 	}
 }
+
+module PN532 {
+	export const enum Tests {
+		Communication = 0X00,
+		ROM = 0X01,
+		RAM = 0X02,
+		Polling = 0X04,
+		Echo = 0X05,
+		PresenceDetection = 0X06,
+		Antenna = 0X07
+	}
+}
+
 
 module.exports = PN532;
